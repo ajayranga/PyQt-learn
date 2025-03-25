6813b99649f531116bb5ad85e03ea31effc85eca
Ajay <ajayranga@talaura.ai>
2025-03-25 13:53:16 +0530

    add button on UI

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
-from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+from PyQt6.QtCore import QSize
 import sys
 
 
-class Docker(QMainWindow):
-    def __init__(self, ):
+class MainWindow(QMainWindow):
+    def __init__(self):
         super().__init__()
         self.setup_ui()
 
     def setup_ui(self):
-        window = QWidget(self)
-        window.show()
+        self.setWindowTitle("Docker")
+        btn = QPushButton("Push Me")
+        self.setCentralWidget(btn)
+        self.setFixedSize(QSize(480, 320))
 
 
 def main():
     app = QApplication(sys.argv)
-    docker = Docker()
+    docker = MainWindow()
     docker.show()
-    app.exec()
+    sys.exit(app.exec())
 
 
 if __name__ == "__main__":
