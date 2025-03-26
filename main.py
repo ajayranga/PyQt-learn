aa686762ee9a83c999e81ed7d29996e9dd6244cf
Ajay <ajayranga@talaura.ai>
2025-03-26 13:33:03 +0530

    qt creator UI

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+from PyQt5.uic import loadUi
 from PyQt5.QtWidgets import (
     QMainWindow,
     QGridLayout,
@@ -28,63 +29,18 @@ from widgets.CustomWidget import CustomWidget
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.setWindowTitle("Docker")
         self.setup_ui()
-        self.widget1 = CustomWidget()
-        self.widget2 = CustomWidget()
-        # self.show_widget = False
 
     def setup_ui(self):
-        self.setWindowTitle("Docker")
-
-        horizontal_layout = QHBoxLayout()
-
-        self.button = QPushButton("Open Widget")
-        self.button.setStatusTip("Open Widget")
-        self.button.setFixedSize(QSize(120, 40))
-        self.button.setCheckable(True)
-        self.button.clicked.connect(
-            lambda checked: self.handle_btn_click(self.widget1, self.button)
-        )
-
-        horizontal_layout.addWidget(self.button)
-
-        self.button2 = QPushButton("Open Widget")
-        self.button2.setStatusTip("Open Widget")
-        self.button2.setFixedSize(QSize(120, 40))
-        self.button2.setCheckable(True)
-        self.button2.clicked.connect(
-            lambda checked: self.handle_btn_click(self.widget2, self.button2)
-        )
-
-        horizontal_layout.addWidget(self.button2)
-
-        container = QWidget()
-        container.setLayout(horizontal_layout)
-
-        self.statusBar()
-        self.setCentralWidget(container)
+        self.window = loadUi("views/screen/mainwindow.ui", self)
+        self.window.show()
         self.setMinimumSize(QSize(480, 320))
-
-    def handle_btn_click(self, widget, button):
-        if widget.isVisible():
-            widget.hide()
-            button.setText("Open Widget")
-        else:
-            widget.show()
-            button.setText("Hide Widget")
-        # if self.show_widget:
-        #     self.widget.close()
-        #     self.button.setText("Open Widget")
-        # else:
-        #     self.widget.show()
-        #     self.button.setText("Hide Widget")
-        # self.show_widget = not self.show_widget
 
 
 def main():
     app = QApplication(sys.argv)
     docker = MainWindow()
-    docker.show()
     sys.exit(app.exec())
 
 
